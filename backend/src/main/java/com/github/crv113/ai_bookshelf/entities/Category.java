91b34b8e8511c193316a003c68a31ef05bd016fb
Adrien Corvisier <adrien.corvisier.17@gmail.com>
2026-06-30 09:58:56 +0200

    Ajout category

---
+++ backend/src/main/java/com/github/crv113/ai_bookshelf/entities/Category.java
@@ -0,0 +1,45 @@
+package com.github.crv113.ai_bookshelf.entities;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @NotBlank
+    private String name;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children;
+}
